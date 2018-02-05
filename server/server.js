58acbfa0bf0e2279861c98ded4c418d3c555926a
Frosso Kouroutsidou <frossokourou@gmail.com>
2018-02-06 01:39:47 +0200

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose');
 const {User} = require('./models/user');
@@ -37,6 +38,23 @@ app.get('/todos', (req, res) => {
     res.send({todos});        // sending back an object lets you add fields
   }, (err) => {
     res.status(400).send(err);
+  });
+});
+
+app.get('/todos/:id', (req, res) => {
+  // res.send(req.params);
+  const id = req.params.id;         // the user gives the id via the url /todos/:id
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send('ID not valid');
+  }
+  Todo.findById(id).then((todo) => {
+    if(!todo) {
+      return res.status(404).send();
+    }
+      res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
   });
 });
 
