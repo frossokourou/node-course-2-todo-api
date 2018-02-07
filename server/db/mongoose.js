2af8e02dff393bfd2cf308f5302dbee3987daa37
Frosso Kouroutsidou <frossokourou@gmail.com>
2018-02-07 18:24:50 +0200

    Correction to previous

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose');
 // configuration to use promises, a feature built in the language
 mongoose.Promise = global.Promise;
 // connect to the database -> just provide the url and the name of the db to create
-const url = 'mongodb://frossokourou:mLabHerokuDB12@ds125318.mlab.com:25318/node-todos-api-course' || 'mongodb://localhost:27017/TodoApp';
-mongoose.connect(url);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
   mongoose
